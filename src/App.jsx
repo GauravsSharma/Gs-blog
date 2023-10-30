b4a95ed3e83b9f7e165e484aaf8bb82099d7c8c1
Gaurav Sharma <113518199+GauravsSharma@users.noreply.github.com>
2023-10-30 18:22:20 +0530

    change in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import conf from './conf/conf';
 import './App.css'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { useEffect } from 'react';
+import { login,logout } from './store/authSlice';
+import {Header ,Footer} from './components/index'
 
 function App() {
- console.log(conf.appwriteBacketID); 
-  return (
-   <>
-     <h1>Hello Bhai</h1>
-   </>
+ const [loading, setLoading] = useState(true); 
+ const dispatch = useDispatch();
+ useEffect(()=>{
+    authService.getUser()
+    .then((userData)=>{
+       if(userData){
+        dispatch(login({userData}))
+       }
+       else{
+        dispatch(logout());
+       }
+    })
+    .finally(()=>setLoading(false));
+ },[])
+  return loading?(<div><h1>Loading...</h1></div>):(
+    <div className='min-h-screen flex flex-wrap content-between bg-slate-300'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+        
+    </div>
   )
 }
 
